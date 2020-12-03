0bd2a83e6c410738c66e446ac7774b1e2cc07fa5
Stephen Burke <stephen@recommenderx.com>
2020-12-03 09:12:12 +0000

    Changed to one listcomp

--- 3/3-2.py
+++ 3/3-2.py
@@ -4,10 +4,7 @@ with open('3.in', 'r') as f:
     data = f.read().splitlines()
 
 def trees(right, down):
-    output = []
-    for i, line in enumerate(data[::down]):
-        output.append(line[(i*right)%len(line)])
-    return len([x for x in output[1:] if x == '#'])
+    return len([x for x in [line[(i*right)%len(line)] for i, line in enumerate(data[::down])] if x == '#'])
 
 slopes = [[1,1], [3,1], [5,1], [7,1], [1,2]]
 print(prod([trees(r,d) for r,d in slopes]))
