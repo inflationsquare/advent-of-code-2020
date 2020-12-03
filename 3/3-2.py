5113ad09c43c9f790abfb617fa4ba94116441f49
Stephen Burke <stephen@recommenderx.com>
2020-12-03 07:35:41 +0000

    the first row doesn't have a tree, but we're not meant to check it anyway, so removing that from the final tally

--- 3/3-2.py
+++ 3/3-2.py
@@ -7,7 +7,7 @@ def trees(right, down):
     output = []
     for i, line in enumerate(data[::down]):
         output.append(line[(i*right)%len(line)])
-    return len([x for x in output if x == '#'])
+    return len([x for x in output[1:] if x == '#'])
 
 slopes = [[1,1], [3,1], [5,1], [7,1], [1,2]]
 print(prod([trees(r,d) for r,d in slopes]))
