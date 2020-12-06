17c0ee3d22533d15f2d05629778ce8fbfbdd9664
Stephen Burke <stephen@recommenderx.com>
2020-12-06 14:11:36 +0000

    inline counter

--- 6/6.py
+++ 6/6.py
@@ -7,8 +7,7 @@ with open('6.in', 'r') as f:
 print(sum(len(set(x)) for x in map(lambda x: x.replace('\n', ''), data)))
 
 answer_sets = [[set(y) for y in x.split('\n')] for x in data]
-answer_counts = [Counter(chain.from_iterable(x)) for x in answer_sets]
-tally = sum(sum(1 for y in c.values() if y == len(s)) for c, s in zip(answer_counts, answer_sets))
+tally = sum(sum(1 for y in Counter(chain.from_iterable(s)).values() if y == len(s)) for s in answer_sets)
 
 print(tally)
 
