701242ec1cf669dee954f47df152711c5fe39160
Stephen Burke <stephen@recommenderx.com>
2020-12-11 10:06:47 +0000

    very slow part 2

--- 11/11.py
+++ 11/11.py
@@ -2,8 +2,13 @@ from itertools import product
 from itertools import chain
 from collections import Counter
 
-with open("11.in", "r") as f:
+with open("test.in", "r") as f:
     data = [[y for y in x] for x in f.read().splitlines()]
+
+
+def print_data(d):
+    print("\n".join("".join(x for x in y) for y in d))
+    print("\n")
 
 
 def get_adjacent(d, x, y):
@@ -13,26 +18,47 @@ def get_adjacent(d, x, y):
     return adjacent
 
 
+def get_adjacent_2(d, x, y):
+    indices = [[i, j] for i, j in product(range(len(d[0])), range(len(d))) if [x, y] != [i, j]]
+    chairs = [[i, j] for i, j in indices if d[j][i] != "."]
+    offsets = [[i - x, j - y] for i, j in chairs]
+    line_of_sight = [
+        [i, j, dx, dy] for [[i, j], [dx, dy]] in zip(chairs, offsets) if dx == 0 or dy == 0 or abs(dx) == abs(dy)
+    ]
+
+    directions = [
+        [[i, j, abs(dx) + abs(dy)] for [i, j, dx, dy] in line_of_sight if dx > 0 and dy == 0],
+        [[i, j, abs(dx) + abs(dy)] for [i, j, dx, dy] in line_of_sight if dx < 0 and dy == 0],
+        [[i, j, abs(dx) + abs(dy)] for [i, j, dx, dy] in line_of_sight if dx == 0 and dy > 0],
+        [[i, j, abs(dx) + abs(dy)] for [i, j, dx, dy] in line_of_sight if dx == 0 and dy < 0],
+        [[i, j, abs(dx) + abs(dy)] for [i, j, dx, dy] in line_of_sight if dx > 0 and dy > 0],
+        [[i, j, abs(dx) + abs(dy)] for [i, j, dx, dy] in line_of_sight if dx < 0 and dy < 0],
+        [[i, j, abs(dx) + abs(dy)] for [i, j, dx, dy] in line_of_sight if dx < 0 and dy > 0],
+        [[i, j, abs(dx) + abs(dy)] for [i, j, dx, dy] in line_of_sight if dx > 0 and dy < 0],
+    ]
+
+    return [sorted(l, key=lambda q: q[-1])[0][:2] for l in directions if l]
+
+
 def change(status, adjacent):
     if status == ".":
         return "."
     elif status == "L" and adjacent["#"] == 0:
         return "#"
-    elif status == "#" and adjacent["#"] >= 4:
+    elif status == "#" and adjacent["#"] >= 5:
         return "L"
     else:
         return status
 
 
-def print_data(d):
-    print("\n".join("".join(x for x in y) for y in d))
-    print("\n")
+indices = [*product(range(len(data[0])), range(len(data)))]
+nearest_neighbours = {(x, y): get_adjacent_2(data, x, y) for x, y in indices}
 
 
 def stabilise(data):
     new_data = [[x for x in "." * len(data[0])] for x in range(len(data))]
-    for x, y in product(range(len(data[0])), range(len(data))):
-        adjacent = Counter(data[j][i] for i, j in get_adjacent(data, x, y))
+    for x, y in indices:
+        adjacent = Counter(data[j][i] for i, j in nearest_neighbours[(x, y)])
         new_data[y][x] = change(data[y][x], adjacent)
     if new_data == data:
         return Counter(chain.from_iterable(new_data))
