bd1e0f359096fa47eda1ad4570721f8d24e119c5
Stephen Burke <stephen@recommenderx.com>
2020-12-13 11:49:43 +0000

    remove timing

--- 13/13.py
+++ 13/13.py
@@ -1,5 +1,4 @@
 from math import prod
-import time
 
 with open("13.in") as f:
     data = f.read().splitlines()
@@ -20,8 +19,6 @@ def chinese_remainder(n, a):
     return total % p
 
 
-t0 = time.time()
 inputs = [(int(x), int(x) - p) for p, x in enumerate(data[1].split(",")) if x != "x"]
 
 print(chinese_remainder([x[0] for x in inputs], [x[1] for x in inputs]))
-print(time.time() - t0)
