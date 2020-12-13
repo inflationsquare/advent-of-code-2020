e9b52ef4f1880d9428afc4615844fb1f71423657
Stephen Burke <stephen@recommenderx.com>
2020-12-13 10:55:29 +0000

    Day 13 solutions

---
+++ 13/13.py
@@ -0,0 +1,28 @@
+from math import prod
+from sympy import mod_inverse
+import time
+
+with open("13.in") as f:
+    data = f.read().splitlines()
+
+earliest_start = int(data[0])
+buses = [int(x) for x in data[1].split(",") if x != "x"]
+
+departing = [(-1 * earliest_start) % x for x in buses]
+
+earliest_bus = sorted([[x, y] for x, y in zip(buses, departing)], key=lambda b: b[1])[0]
+
+print(prod(earliest_bus))
+
+
+def chinese_remainder(n, a):
+    p = prod(n)
+    total = sum(y * mod_inverse(p // x, x) * (p // x) for x, y in zip(n, a))
+    return total % p
+
+
+t0 = time.time()
+inputs = [(int(x), int(x) - p) for p, x in enumerate(data[1].split(",")) if x != "x"]
+
+print(chinese_remainder([x[0] for x in inputs], [x[1] for x in inputs]))
+print(time.time() - t0)
